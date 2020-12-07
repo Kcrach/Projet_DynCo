ca9408e48116ae8507db2d6a115563d725aa7e2e
Louis Guillotin <louisguillotin.lg@gmail.com>
2020-12-07 11:50:48 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from pyld import jsonld
+import codecs
+import json
+
+# read file
+with open('dataset.json') as myfile:
+    doc = json.load(myfile)
+
+# read file
+with open('ctx.json') as myfile:
+    context = json.load(myfile)
+
+# compact a document according to a particular context
+# see: https://json-ld.org/spec/latest/json-ld/#compacted-document-form
+expanded = jsonld.expand(doc,  {'expandContext': context})
+
+print(json.dumps(expanded, indent=2))
+
+with open('expanded.json', 'w') as f:
+    json.dump(expanded,f,indent=2)
+
+nquads = jsonld.to_rdf(expanded, {'format' : 'application/n-quads'})
+
+with codecs.open('nquads.txt', 'w', 'utf-8') as f:
+    f.write(nquads)
